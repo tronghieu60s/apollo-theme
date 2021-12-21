56efd7ef21723f8427e95e6479295dbc2bc0b511
Trong Hieu <tronghieu60s@gmail.com>
2021-12-21 14:59:06 +0700

    update slide feedback

--- public/js/main.js
+++ public/js/main.js
@@ -1,3 +1,11 @@
 $(".ap-feedback-slide").slick({
-  dots: true,
+  fade: true,
+  infinite: true,
+  infinite: true,
+  slidesToShow: 1,
+  slidesToScroll: 1,
+  prevArrow:
+    '<span class="ap-feedback-slide-arrow ap-feedback-slide-arrow-prev"><img src="./public/images/pre.svg" alt=""></span>',
+  nextArrow:
+    '<span class="ap-feedback-slide-arrow ap-feedback-slide-arrow-next"><img src="./public/images/next.svg" alt=""></span>',
 });
