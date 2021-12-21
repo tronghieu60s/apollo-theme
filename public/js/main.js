243e48a14bdbaf89c2dd2a76cae982ef26f41a88
Trong Hieu <tronghieu60s@gmail.com>
2021-12-21 20:12:26 +0700

    responsive slick

--- public/js/main.js
+++ public/js/main.js
@@ -1,24 +1,38 @@
 $(document).ready(function () {
   $(".ap-feedback-nav").slick({
     dots: false,
-    arrows: false,
     infinite: true,
     slidesToShow: 5,
     slidesToScroll: 1,
-    centerMode:true,
+    centerMode: true,
     centerPadding: 0,
     focusOnSelect: true,
     asNavFor: ".ap-feedback-slide",
+    prevArrow:
+      '<span class="ap-feedback-nav-arrow ap-feedback-nav-arrow-prev d-block d-md-none"><img src="./public/images/pre.svg" alt=""></span>',
+    nextArrow:
+      '<span class="ap-feedback-nav-arrow ap-feedback-nav-arrow-next d-block d-md-none"><img src="./public/images/next.svg" alt=""></span>',
+    responsive: [
+      {
+        breakpoint: 1200,
+        settings: { slidesToShow: 3 },
+      },
+      {
+        breakpoint: 768,
+        settings: { slidesToShow: 1 },
+      },
+    ],
   });
   $(".ap-feedback-slide").slick({
+    dots: false,
     fade: true,
     infinite: true,
     slidesToShow: 1,
     slidesToScroll: 1,
     asNavFor: ".ap-feedback-nav",
     prevArrow:
-      '<span class="ap-feedback-slide-arrow ap-feedback-slide-arrow-prev"><img src="./public/images/pre.svg" alt=""></span>',
+      '<span class="ap-feedback-slide-arrow ap-feedback-slide-arrow-prev d-none d-md-block"><img src="./public/images/pre.svg" alt=""></span>',
     nextArrow:
-      '<span class="ap-feedback-slide-arrow ap-feedback-slide-arrow-next"><img src="./public/images/next.svg" alt=""></span>',
+      '<span class="ap-feedback-slide-arrow ap-feedback-slide-arrow-next d-none d-md-block"><img src="./public/images/next.svg" alt=""></span>',
   });
 });
